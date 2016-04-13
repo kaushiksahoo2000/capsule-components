0c2f7458124612254ad28cb575000c4c42a7dc36
Joseph Martin <jpranaymartin@gmail.com>
2016-04-12 18:50:43 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: [
+    './dev/current.js'
+  ],
+  output: {
+    path: __dirname + '/dev/',
+    filename: 'bundle.js'
+  },
+  debug: true,
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: __dirname + '/dev/',
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-1', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './client/'
+  }
+};
